0ab3c5ce5c4d604898c0ef43618a8d799d1462f4
Jerquanus Swann <jerquanus@gmail.com>
2020-11-19 02:14:28 -0500

    text box

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,12 @@
 
 
-var today = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-$("#currentDay").text(today)
+var today = moment().format('dddd, MMMM Do YYYY, h:mm:ss a')
+let todayTwo = $('#currentDay').text(today)
+console.log('today ', today)
+console.log('todayTwo ', todayTwo)
+
+
+// var save = 
+
+// localStorage.setItem();
 
