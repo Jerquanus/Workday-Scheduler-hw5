7dd81034612362cd20bf123fa1e5fa5b0dcc4c9d
Jerquanus Swann <jerquanus@gmail.com>
2020-11-21 01:00:34 -0500

    it works

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,46 @@
+
+
+
+// when this page loads the data loads go to local store
+window.onload = function() {
+    console.log('localStorage');
+    var allData= localStorage;
+    console.log('allData', allData);
+    var allInputs = document.getElementsByClassName('allInputs')
+    console.log(allInputs[0].name)
+for (let i = 0; i < allInputs.length; i++) {
+      $(allInputs[i]).val (allData[allInputs[i].name])
+
+}
+};
+
 var timeDisplay = $('#currentDay')
+
 
 function currentTime() {
     var today = moment();
     timeDisplay.text(today.format('MMM DD, YYYY [at] hh:mm:ss a'));
-    var timeRightNow = today.hours()
+    // var timeRightNow = today.hours('h')
 
-    console.log
 
     for(var i = 9; i <= 17; i++) {
         var hourEl = document.getElementById(i.toString());
         var id = parseInt(hourEl.id)
 
-        // hourEl.classList.add('past')
+        if (id < timeRightNow ) {
+            hourEl.classList.add('past');
+        } else if (id === timeRightNow) {
+            hourEl.classList.add('present');
+        }   else (id > timeRightNow) 
+        {   hourEl.classList.add('future');
+            }
 
         // less than, equal to or greater than
-        if(id < timeRightNow) {
-            hourEl.classList.add('past')
-        }
-    
-        
-        // console.log(id,timeRightNow)
-        
+        // if(id < timeRightNow) {
+        //     hourEl.classList.add('past')
+        // }
+
+        // }; 
     }
 
 
@@ -29,7 +49,23 @@ function currentTime() {
 
 
 
-
+// put inputs into an array
+var saveButton =document.getElementsByClassName('saveBtn')
+var allInputs = document.getElementsByClassName('allInputs')
+for (let i = 0; i < allInputs.length; i++) {
+  
+    // adding event listen to do whatever it needs
+  $(allInputs[i]).change((event) => {
+   
+    console.log(i, event)
+    var word = event.target.value
+      $(allInputs[i]).val(word)
+      localStorage.setItem(event.target.name, word)
+  })
+  $(saveButton[i].click((event)=>{
+    $(allInputs[i]).val('')
+  }))
+}
 
 currentTime()
 
@@ -42,5 +78,5 @@ currentTime()
 
 
 
-// localStorage.setItem();
+
 
