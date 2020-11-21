9c1febc623293a52aa260b76c8e83ef6b5af16f7
Jerquanus Swann <jerquanus@gmail.com>
2020-11-21 15:36:02 -0500

    Its not working

--- Develop/script.js
+++ Develop/script.js
@@ -20,33 +20,29 @@ var timeDisplay = $('#currentDay')
 function currentTime() {
     var today = moment();
     timeDisplay.text(today.format('MMM DD, YYYY [at] hh:mm:ss a'));
-    // var timeRightNow = today.hours('h')
+    var timeRightNow = today.hours('h')
 
-
+    // while current time is running add classes to each time block
+    
     for(var i = 9; i <= 17; i++) {
         var hourEl = document.getElementById(i.toString());
         var id = parseInt(hourEl.id)
 
         if (id < timeRightNow ) {
             hourEl.classList.add('past');
-        } else if (id === timeRightNow) {
+
+        } else if (id = timeRightNow) {
             hourEl.classList.add('present');
-        }   else (id > timeRightNow) 
-        {   hourEl.classList.add('future');
-            }
-
-        // less than, equal to or greater than
-        // if(id < timeRightNow) {
-        //     hourEl.classList.add('past')
-        // }
-
-        // }; 
+            
+        }   else { (id > timeRightNow)
+            hourEl.classList.add('future');
+        }
     }
 
 
 }
-
-
+// stores current time
+currentTime()
 
 
 // put inputs into an array
@@ -67,7 +63,7 @@ for (let i = 0; i < allInputs.length; i++) {
   }))
 }
 
-currentTime()
+
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Workday-Scheduler-hw5
+
+Workday Scheduler
+
+Installation:
+    Open index.html file. You'll find the javascript and css sheets in the Develop folder.
+
+
+Usage:
+The goal was to create a functioning wordday schdeuler. The requirements were for the time to display correctly on the screen and that the time blocks were color coordinated according to past, present, or future events. The time blocks also had to be editable and be able to store those events locally so that if you refreshed the page the events would still stay on the page.
+
+
+
+
+License: Feel free to use how you need.
